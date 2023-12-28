7e9aa9eb7ebadd95515491fe85e2484f26bd5577
LARRYSALSA <75591878+LARRYICLOUD@users.noreply.github.com>
2023-12-27 21:31:22 -0500

    Agregar archivos de proyecto.

---
+++ Estructura/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estructura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("YO SOY UN PROGRAMADOR Y EXPERTO EN REDES EXCELENTE Y RECONOCIDO");
+            //Instanciando a la estructura
+            Alumno alumno1 = new Alumno();
+
+            //Asignando valor a un campo
+            alumno1.Nombre = "LARRYSALSA";
+            //Mostrando el valor del campo "nombre"
+            Console.WriteLine($"El nombre del alumno es:  {alumno1.Nombre}");
+
+        }
+    }
+    struct Alumno
+    {
+        //Campos
+        string nombre;
+        string apellido;
+        double calificacion;
+        bool alergias;
+        string numeroTelefono;
+
+        //Creamos una propiedad para nombre
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+
+
+
+
+    }
+}
